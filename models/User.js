32a7f2c1da2f697fefd1b028dbecbfafbf910fda
Hyerin Sung <hyerinx@naver.com>
2023-11-06 06:33:29 +0900

    노드 리액트 기초 강의 #10 Bcrypt로 비밀번호 암호화하기

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     id: {
@@ -24,6 +26,21 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next){
+    let user = this;
+    if(user.isModified('password')) {
+        //비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(error, salt) {
+            if(error) return next(error);
+            bcrypt.hash(user.password, salt, function(error, hash) {
+                if(error) return next(error);
+                user.password = hash;
+                next();
+            })
+        })
     }
 })
 
