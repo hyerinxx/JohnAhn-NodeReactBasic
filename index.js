bd4303fe794b32a101feef1880aaa07386aa953f
Hyerin Sung <hyerinx@naver.com>
2023-11-07 02:21:38 +0900

    노드 리액트 기초 강의 #11 로그인 기능 with Bcrypt (1) #12 토큰 생성 with jsonwebtoken

--- index.js
+++ index.js
@@ -1,34 +1,87 @@
-const express = require('express')
-const app = express()
-const port = 5000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const express = require('express');
+const app = express();
+const port = 5000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-const mongoose = require('mongoose')
-const config = require('./config/key')
+const mongoose = require('mongoose');
+const config = require('./config/key');
 mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
-.catch(error => console.log(error))
+.catch(error => console.log(error));
 
-const {User} = require("./models/User");
 const bodyParser = require('body-parser');
 //application/x-www-form-urlencoded 타입의 데이터를 분석하여 가져올 수 있도록 한다.
 app.use(bodyParser.urlencoded({extended: true}));
 //application/json 타입의 데이터를 분석하여 가져올 수 있도록 한다.
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세요! 😊'))
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+const {User} = require("./models/User");
+
+app.get('/', (req, res) => res.send('Hello World! 안녕하세요! 😊'));
 
 app.post('/register', async (req, res) => {
     //회원가입 시 필요한 정보를 client에서 가져와
     //이를 데이터베이스에 삽입한다.
 
     //body parser를 이용하여 body에 담긴 정보를 가져온다.
-    const user = new User(req.body)
+    const user = new User(req.body);
 
     //mongoDB 메서드 save()를 이용하여 정보를 user 모델에 저장한다.
-    const result = await user.save().then(()=>{
-        res.status(200).json({success: true})
-    }).catch((error)=>{
-        res.json({success: false, error})
+    const result = await user.save()
+    .then(()=>{
+        res.status(200).json({success: true});
+    }).catch((error) => {
+        res.json({success: false, error});
     })
-})
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        //사용자가 전송한 이메일 정보가 데이터베이스에 존재하는지 찾는다.
+        const user = await User.findOne({email: req.body.email});
+        
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "이메일 정보를 찾을 수 없습니다. 다시 입력하세요."
+            });
+        }
+        
+        //이메일 정보가 존재한다면, 비밀번호 정보가 일치하는지 확인힌다.
+        const isCorrect = await user.comparePassword(req.body.password);
+        
+        if(!isCorrect){
+            return res.json({
+                loginSuccess: false,
+                message: "비밀번호가 틀렸습니다."
+            });
+        }
+        
+        //비밀번호가 일치한다면, 토큰을 생성한다.
+        const token = await user.generateToken();
+
+        if(token) {
+            //토큰을 저장한다. (저장소: Cookie, Localstorage 등)
+            res.cookie("x_auth", token)
+            .status(200)
+            .json({
+                loginSuccess: true,
+                userId: user._id
+            });
+        } else {
+            return res.json({
+                loginSuccess: false,
+                message: "토큰 생성을 실패하였습니다."
+            });
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            loginSuccess: false,
+            message: "서버에서 오류가 발생했습니다."
+        });
+    }
+});
--- models/User.js
+++ models/User.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
-    id: {
-        type: String,
-        maxlength: 50
-    },
-    password: {
-        type: String,
-        maxlength: 12
-    },
     email: {
         type: String,
         trim: true,
         unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    firstname: {
+        type: String,
+        maxlength: 50
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
     },
     role: {
         type: Number,
@@ -27,23 +32,48 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
-})
+});
 
-userSchema.pre('save', function(next){
+userSchema.pre('save', async function(next){
     let user = this;
     if(user.isModified('password')) {
-        //비밀번호 암호화
-        bcrypt.genSalt(saltRounds, function(error, salt) {
-            if(error) return next(error);
-            bcrypt.hash(user.password, salt, function(error, hash) {
-                if(error) return next(error);
-                user.password = hash;
-                next();
-            })
-        })
+        try {
+            //비밀번호 암호화
+            const salt = await bcrypt.genSalt(saltRounds);
+            const hash = await bcrypt.hash(user.password, salt);
+            user.password = hash;
+            next();
+        } catch(error) {
+            return next(error);
+        }
+    } else {
+        next();
     }
-})
+});
 
-const User = mongoose.model('User', userSchema)
+userSchema.methods.comparePassword = async function(inputPassword) {
+    try {
+        return await bcrypt.compare(inputPassword, this.password);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
 
-module.exports = {User}
+
+userSchema.methods.generateToken = async function() {
+    let user = this;
+
+    try {
+        //jsonwebtoken을 이용하여 token 생성하기
+        let token = jwt.sign(user._id.toHexString(), 'secretToken');
+        user.token = token;
+        await user.save();
+        return token;
+    } catch(error) {
+        throw new Error(error);
+    }
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
