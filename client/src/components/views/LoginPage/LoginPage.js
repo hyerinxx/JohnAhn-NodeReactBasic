85febdba2c04962ecf06a8b2611ac5e690539d06
Hyerin Sung <hyerinx@naver.com>
2023-11-12 18:41:51 +0900

    노드 리액트 기초 강의 #31 회원가입 페이지

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../../../_actions/user_action';
-import { useNavigate } from "react-router-dom";
 
 function LoginPage(props) {
   const dispatch = useDispatch();
