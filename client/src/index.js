5995e5497b629e404dee96380572431cf51cfc30
Hyerin Sung <hyerinx@naver.com>
2023-11-12 14:25:03 +0900

    노드 리액트 기초 강의 #27 Redux UP!!!!!

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
 import './index.css';
 import 'antd/dist/antd.css';
 import App from './App';
 
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(Reducer, 
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
